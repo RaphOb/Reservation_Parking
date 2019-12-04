5b9ca007a105672ff18021a7ce81e395e5b22ed9
Augustin <augu.hourlier@gmail.com>
2019-12-04 16:52:41 +0100

    Test to export JSON

--- maven-quest-ebenus-web/src/main/java/com/cours/ebenus/servlets/CrudUserServlet.java
+++ maven-quest-ebenus-web/src/main/java/com/cours/ebenus/servlets/CrudUserServlet.java
@@ -5,6 +5,8 @@
  */
 package com.cours.ebenus.servlets;
 
+import java.io.File; 
+import java.io.FileWriter;
 import java.io.IOException;
 import java.util.List;
 
@@ -16,6 +18,8 @@ import javax.servlet.http.HttpSession;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
 
 import com.cours.ebenus.dao.entities.Utilisateur;
 import com.cours.ebenus.service.IServiceFacade;
@@ -30,6 +34,9 @@ public class CrudUserServlet extends HttpServlet {
 
 	private static final Log log = LogFactory.getLog(LoginServlet.class);
 	private static IServiceFacade service = null;
+	
+	//Pas trouvé de moyen de donner un path relatif avec ~
+	private static String downloadPath = "/home/augustin/Téléchargements";
 	
     /**
      * Méthode d'initialisation de la Servlet
@@ -93,6 +100,27 @@ public class CrudUserServlet extends HttpServlet {
     	if(request.getParameter("action").equals("exportJSON"))
     	{
     		log.debug("exporting users to JSON");
+    		
+    		/* TEST TO EXPORT AS JSON */
+    		
+    		File file = new File(downloadPath, "export_user.json");
+    		file.createNewFile();
+    		
+    		JSONObject obj = new JSONObject();
+    		obj.put("Name", "crunchify.com");
+    		obj.put("Author", "App Shah");
+     
+    		JSONArray company = new JSONArray();
+    		company.add("Compnay: eBay");
+    		company.add("Compnay: Paypal");
+    		company.add("Compnay: Google");
+    		obj.put("Company List", company);
+     
+    		try (FileWriter writer = new FileWriter(file)) {
+    			writer.write(obj.toJSONString());
+    			System.out.println("\nJSON Object: " + obj);
+    		}
+    		
     	}
     	if(request.getParameter("action").equals("exportXML"))
     	{
